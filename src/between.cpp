67cfaff9f35aa759539b4a1a0aec188b6eb0e59c
= <=>
2024-01-04 03:21:50 -0300

    feat: add between.cpp

---
+++ src/between.cpp
@@ -0,0 +1,17 @@
+#include "between.hpp"
+
+#include <random>
+
+using std::mt19937;
+using std::random_device;
+using std::uniform_real_distribution;
+
+double between(const double start, const double end) {
+  random_device device;
+
+  mt19937 mersenne_twister(device());
+
+  uniform_real_distribution<double> distribution(start, end);
+
+  return distribution(mersenne_twister);
+}
